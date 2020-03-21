a7e4be6ef761631de46e3e36e0b25ea9e5a7caf4
Austin Dewey <deweya964@gmail.com>
2020-03-21 04:39:52 -0500

    allowing app to be run in eap container

--- src/main/java/com/austindewey/S2iDemo.java
+++ src/main/java/com/austindewey/S2iDemo.java
@@ -2,10 +2,17 @@ package com.austindewey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class S2iDemo {
+public class S2iDemo extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(S2iDemo.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(S2iDemo.class);
     }
 }
